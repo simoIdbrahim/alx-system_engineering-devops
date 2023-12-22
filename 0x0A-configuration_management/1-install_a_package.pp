9edfac52d0e9cb50dc104eb14038a15c4501f840
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-12-22 05:07:39 -0800

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,10 @@
-#!/usr/bin/pup
 # install flask from pip3 package.
+package { 'python3-pip':
+  ensure => present,
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip']
+  require  => Package['python3-pip'],
 }
