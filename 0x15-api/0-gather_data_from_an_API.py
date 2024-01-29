8aadda1c7aceada9ddc755a7ba1bd0bb8b51be3e
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-01-29 07:11:58 -0800

    add

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,31 +2,31 @@
 """ script that, using this REST API, for a given employee ID,
 returns information about his/her todo list progress """
 
-import requests
 import sys
+import requests
 
 
 def gather_data_from_api(employee_id):
     base_url = "https://jsonplaceholder.typicode.com"
 
     # Fetch employee data
-    employee_url = f"{base_url}/users/{employee_id}"
+    employee_url = "{}/users/{}".format(base_url, employee_id)
     employee_response = requests.get(employee_url)
 
     if employee_response.status_code != 200:
-        print(f"Employee Name: Incorrect\n\n(25 chars long)")
+        print("Employee Name: Incorrect\n\n(25 chars long)")
         sys.exit(1)
 
     employee_data = employee_response.json()
     employee_name = employee_data.get('name')
 
-    # Fetch TODO list for the employee
-    todo_url = f"{employee_url}/todos"
+    # Fetch todo list for the employee
+    todo_url = "{}/todos".format(employee_url)
     todo_response = requests.get(todo_url)
 
     if todo_response.status_code != 200:
-        print(
-            f"Error: Unable to fetch TODO list for employee ID {employee_id}")
+        print("Error: Unable to fetch TODO list for employee ID {}"
+              .format(employee_id))
         sys.exit(1)
 
     todo_list = todo_response.json()
@@ -36,13 +36,11 @@ def gather_data_from_api(employee_id):
     completed_tasks = [task for task in todo_list if task.get('completed')]
 
     # Display results
-     print(
-        f"Employee {employee_name} is done with tasks("
-        f"{len(completed_tasks)}/{total_tasks}):"
-    )
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+          len(completed_tasks), total_tasks))
 
     for task in completed_tasks:
-        print(f"\t {task.get('title')}")
+        print("\t {}".format(task.get('title')))
 
 
 if __name__ == "__main__":
